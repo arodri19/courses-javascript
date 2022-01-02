b8cc5ee1dc48b59d1682e326604580088412974d
arodri19 <andre.rodrigues19@outlook.com.br>
2022-01-02 13:17:24 -0300

    Termino curso Node.js: Refresh Tokens e confirmação de cadastro

--- src/posts/posts-controlador.js
+++ src/posts/posts-controlador.js
@@ -2,7 +2,7 @@ const Post = require('./posts-modelo');
 const { InvalidArgumentError, InternalServerError } = require('../erros');
 
 module.exports = {
-  adiciona: async (req, res) => {
+  async adiciona(req, res) {
     try {
       const post = new Post(req.body);
       await post.adiciona();
@@ -11,15 +11,13 @@ module.exports = {
     } catch (erro) {
       if (erro instanceof InvalidArgumentError) {
         res.status(422).json({ erro: erro.message });
-      } else if (erro instanceof InternalServerError) {
-        res.status(500).json({ erro: erro.message });
       } else {
         res.status(500).json({ erro: erro.message });
       }
     }
   },
 
-  lista: async (req, res) => {
+  async lista (req, res) {
     try {
       const posts = await Post.lista();
       res.send(posts);
