eb85b3d4f134e11e0716b9f866c3862f153f103b
andre.queiroz <andre.queiroz@svatech.com.br>
2022-03-28 15:18:42 -0300

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '../models/account'
+
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add: (account: AddAccountModel) => AccountModel
+}
+
+// Modelo de classe de banco de dados - AccountModel
+// AddAccountModel - especifico desta interface
