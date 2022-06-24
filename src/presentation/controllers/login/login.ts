045d11dfe174c6e07fdf091a849107e9d14de36a
André Rodrigues Queiroz <andre.rodrigues19@outlook.com.br>
2022-06-23 22:42:12 -0300

    Revert "teste"
    This reverts commit 09cbfe2a8e8d820cc7f7bfca1b02f4ff62cf2930.

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -19,7 +19,6 @@ export class LoginController implements Controller {
     }
     const isValid = this.emailValidator.isValid(email)
     if (!isValid) {
-      console.log('teste')
       return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
     }
   }
