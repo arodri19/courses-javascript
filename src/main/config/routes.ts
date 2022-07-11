29b5184225dc68ce00f9d68de43c939a28f93e6f
andre.queiroz <andre.queiroz@svatech.com.br>
2022-07-11 11:52:19 -0300

    chore: enable debug with vscode and fix a part about path route

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,14 +1,14 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import path from 'path'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
-  app.use('/api',router)
+  app.use('/api', router)
 
-  readdirSync(path.join(__dirname,'..','routes')).map(async file => {
-    if (!file.includes('.test.')) {
-      (await import(path.join('..','routes',file))).default(router)
+  readdirSync(join(__dirname, '../routes')).map(async file => {
+    if (!(file.includes('.test.') || file.includes('.map'))) {
+      (await import(join('../routes', file))).default(router)
     }
   })
 }
