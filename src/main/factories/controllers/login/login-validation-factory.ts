c0d2ffeee1fe7305dc8ce200ad23ce990633b2c1
andre.queiroz <andre.queiroz@svatech.com.br>
2022-07-19 15:47:09 -0300

    feat: add AddSurvey composition

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,4 +1,3 @@
-
 import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
 import { Validation } from '../../../../presentation/protocols/validation'
 import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
