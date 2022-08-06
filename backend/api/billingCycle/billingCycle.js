eab43544a7d7cdafac1d2d9fd7cf0b63c54493c9
André Rodrigues Queiroz <andre.rodrigues19@outlook.com.br>
2022-08-06 13:34:16 -0300

    fix: adjust packages and later fix code

--- backend/api/billingCycle/billingCycle.js
+++ backend/api/billingCycle/billingCycle.js
@@ -1,19 +1,19 @@
-const restful = require('node-restful')
-const mongoose = restful.mongoose
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
-const creditSchema = new mongoose.Schema({
+const creditSchema = new Schema({
   name:{type:String, required:true},
   value:{type:Number, min:0, required:true}
 })
 
-const debtSchema = new mongoose.Schema({
+const debtSchema = new Schema({
   name:{type:String, required: true},
   value:{type: Number, min:0, required: [true,'Informe o valor do débito!']},
   status:{type:String, required: false, uppercase:true,
   enum:['PAGO','PENDENTE','AGENDADO']}
 })
 
-const billingCycleSchema = new mongoose.Schema({
+const billingCycleSchema = new Schema({
   name:{type:String,required:true},
   month:{type:Number, min:1, max:12, required:true},
   year:{type:Number, min:1970, max:2100, required:true},
@@ -21,4 +21,4 @@ const billingCycleSchema = new mongoose.Schema({
   debts:[debtSchema]
 })
 
-module.exports = restful.model('BillingCycle',billingCycleSchema)
+module.exports = mongoose.model('BillingCycle',billingCycleSchema)
--- backend/api/user/user.js
+++ backend/api/user/user.js
@@ -1,11 +1,10 @@
-const restful = require('node-restful')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
-const mongoose = restful.mongoose
-
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, min: 6, max: 12, required: true }
 })
 
-module.exports = restful.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)
