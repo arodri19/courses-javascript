d5c48c62ec1029b67c1dbdd4eef006e25be1b017
André Rodrigues Queiroz <andre.rodrigues19@outlook.com.br>
2022-10-02 21:05:13 -0300

    feat: add SurveyResult query and mutation

---
+++ src/main/graphql/resolvers/survey-result.ts
@@ -0,0 +1,15 @@
+import { adaptResolver } from '@/main/adapters'
+import { makeLoadSurveyResultController, makeSaveSurveyResultController } from '@/main/factories/controllers'
+
+export default {
+  Query: {
+    surveyResult: async (parent: any, args: any) => {
+      return adaptResolver(makeLoadSurveyResultController(), args)
+    }
+  },
+  Mutation: {
+    saveSurveyResult: async (parent: any, args: any) => {
+      return adaptResolver(makeSaveSurveyResultController(), args)
+    }
+  }
+}
