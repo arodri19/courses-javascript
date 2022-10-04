f43090f87f7c0a62037aa69809df5eddb0f660bf
André Rodrigues Queiroz <andre.rodrigues19@outlook.com.br>
2022-10-03 21:41:22 -0300

    test: create surveyResult case

--- src/main/graphql/resolvers/survey.ts
+++ src/main/graphql/resolvers/survey.ts
@@ -3,8 +3,8 @@ import { makeLoadSurveysController } from '@/main/factories/controllers'
 
 export default {
   Query: {
-    surveys: async () => {
-      return adaptResolver(makeLoadSurveysController())
+    surveys: async (parent: any, args: any, context: any) => {
+      return adaptResolver(makeLoadSurveysController(), args, context)
     }
   }
 }
