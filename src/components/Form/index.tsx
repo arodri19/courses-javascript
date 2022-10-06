544b66d0745071231970582395a6da3f0cc3266d
andre.queiroz <andre.queiroz@svatech.com.br>
2022-10-06 11:55:27 -0300

    feat: finishing class 5 alura

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { ITask } from "../../types/tasks";
 import Button from "../Button";
 import style from './Form.module.scss'
+import { v4 as uuidv4 } from 'uuid'
 
-class Form extends React.Component {
+class Form extends React.Component<{
+    setTasks: React.Dispatch<React.SetStateAction<ITask[]>>
+}> {
+    state = {
+        task: "",
+        time: "00:00"
+    }
+    addTask(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        this.props.setTasks(oldTasks => 
+            [
+                ...oldTasks, 
+                {
+                    ...this.state,
+                    selected: false,
+                    completed: false,
+                    id: uuidv4()
+                }
+            ]
+        );
+        this.setState({
+            task: "",
+            time: "00:00"
+        })
+    }
+
     render () {
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">
                         Adicione um novo estudo
@@ -14,6 +41,8 @@ class Form extends React.Component {
                         type="text" 
                         name="task" 
                         id="task" 
+                        value={this.state.task}
+                        onChange={event => this.setState({...this.state, task: event.target.value})}
                         placeholder="O que você quer estudar" 
                         required
                     />
@@ -26,13 +55,15 @@ class Form extends React.Component {
                             type="time" 
                             step="1"
                             name="time"
+                            value={this.state.time}
+                            onChange={event => this.setState({...this.state, time: event.target.value})}
                             id="time"
                             min="00:00:00"
                             max="01:30:00"
                             required
                         />
                 </div>
-                <Button
+                <Button type="submit"
                     texto="Adicionar"
                 >
                     Adicionar
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,18 +1,10 @@
-import React from "react";
+import { ITask } from "../../types/tasks";
 import Item from "./Item";
-import style from './List.module.scss'
+import style from './List.module.scss';
 
-function List() {
-    const tasks = [{
-        task: 'React',
-        time: '02:00:00'
-    }, {
-        task: 'Javascript',
-        time: '01:00:00'
-    }, {
-        task: 'Typescrypt',
-        time: '03:00:00'
-    }]
+
+
+function List({ tasks }: { tasks: ITask[]}) {
     return (
         <aside className={style.taskList}>
             <h2>Estudos do dia</h2>
