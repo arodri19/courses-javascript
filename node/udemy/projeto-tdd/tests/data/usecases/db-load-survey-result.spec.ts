6b43176aa6b5a497e3f796e94604eab69f64b06d
André Rodrigues Queiroz <andre.rodrigues19@outlook.com.br>
2022-10-22 17:24:06 -0300

    fix: adjust gitignore file

---
+++ node/udemy/projeto-tdd/tests/data/usecases/db-load-survey-result.spec.ts
@@ -0,0 +1,80 @@
+import { LoadSurveyResultRepositorySpy, LoadSurveyByIdRepositorySpy } from '@/../tests/data/mocks'
+import { DbLoadSurveyResult } from '@/data/usecases/'
+import MockDate from 'mockdate'
+import { faker } from '@faker-js/faker'
+import { throwError } from '@/tests/domain/mocks'
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositorySpy: LoadSurveyResultRepositorySpy
+  loadSurveyByIdRepositorySpy: LoadSurveyByIdRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositorySpy = new LoadSurveyResultRepositorySpy()
+  const loadSurveyByIdRepositorySpy = new LoadSurveyByIdRepositorySpy()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositorySpy, loadSurveyByIdRepositorySpy)
+  return {
+    sut,
+    loadSurveyResultRepositorySpy,
+    loadSurveyByIdRepositorySpy
+  }
+}
+
+let surveyId: string
+let accountId: string
+
+describe('DbLoadSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  beforeEach(() => {
+    surveyId = faker.datatype.uuid()
+    accountId = faker.datatype.uuid()
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+  test('Should call LoadSurveyResultRepository with correct values', async () => {
+    const { sut, loadSurveyResultRepositorySpy } = makeSut()
+    await sut.load(surveyId, accountId)
+    expect(loadSurveyResultRepositorySpy.surveyId).toBe(surveyId)
+    expect(loadSurveyResultRepositorySpy.accountId).toBe(accountId)
+  })
+  test('Should throw if LoadSurveyRepository throws', async () => {
+    const { sut, loadSurveyResultRepositorySpy } = makeSut()
+    jest.spyOn(loadSurveyResultRepositorySpy, 'loadBySurveyId').mockImplementationOnce(throwError)
+    const promise = sut.load(surveyId, accountId)
+    await expect(promise).rejects.toThrow()
+  })
+  test('Should call LoadSurveyByIdRepository if LoadSurveyResultRepository returns null', async () => {
+    const { sut, loadSurveyResultRepositorySpy, loadSurveyByIdRepositorySpy } = makeSut()
+    loadSurveyResultRepositorySpy.result = null
+    await sut.load(surveyId, accountId)
+    expect(loadSurveyByIdRepositorySpy.id).toBe(surveyId)
+  })
+  test('Should return surveyResultMode with all answers with count 0 if LoadSurveyResultRepository returns null', async () => {
+    const { sut, loadSurveyResultRepositorySpy, loadSurveyByIdRepositorySpy } = makeSut()
+    loadSurveyResultRepositorySpy.result = null
+    const surveyResult = await sut.load(surveyId, accountId)
+    const { result } = loadSurveyByIdRepositorySpy
+    expect(surveyResult).toEqual({
+      surveyId: result.id,
+      question: result.question,
+      date: result.date,
+      answers: result.answers.map(answer => Object.assign({}, answer, {
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }))
+    })
+  })
+
+  test('Should return surveyResultModel on success', async () => {
+    const { sut, loadSurveyResultRepositorySpy } = makeSut()
+    const surveyResult = await sut.load(surveyId, accountId)
+    expect(surveyResult).toEqual(loadSurveyResultRepositorySpy.result)
+  })
+})
