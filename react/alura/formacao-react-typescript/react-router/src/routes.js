55b5f9e3a1e1af783be1e0a09e313da5ed4ffec9
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2023-02-02 19:51:40 -0300

    feat: terminando aula04 react-router

--- react/alura/formacao-react-typescript/react-router/src/routes.js
+++ react/alura/formacao-react-typescript/react-router/src/routes.js
@@ -1,7 +1,11 @@
+import Footer from 'components/Footer';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -13,9 +17,12 @@ export default function AppRouter() {
           <Route path='/' element={<PaginaPadrao />}>
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre/>} />
+            <Route path='prato/:id' element={<Prato/>} />
           </Route>
-          
+          <Route path='*' element={<NotFound/>} />
         </Routes>
+        <Footer/>
       </Router>
     </main>
   );
