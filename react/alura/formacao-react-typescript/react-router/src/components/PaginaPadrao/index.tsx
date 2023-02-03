dbbbc8c7761222617482fe3517d1a3b0d193ab6d
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2023-02-02 21:33:32 -0300

    feat: terminando aula05

--- react/alura/formacao-react-typescript/react-router/src/components/PaginaPadrao/index.tsx
+++ react/alura/formacao-react-typescript/react-router/src/components/PaginaPadrao/index.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import styles from './PaginaPadrao.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
+import React from 'react';
 
 
-export default function PaginaPadrao() {
+export default function PaginaPadrao({ children } : {children?: React.ReactNode}) {
   return (
     <>
       <header className={styles.header}>
--- react/alura/formacao-react-typescript/react-router/src/pages/Prato/index.tsx
+++ react/alura/formacao-react-typescript/react-router/src/pages/Prato/index.tsx
@@ -1,8 +1,9 @@
 import styles from './Prato.module.scss';
-import { useLocation, useParams, useNavigate } from 'react-router-dom';
-import classNames from 'classnames';
+import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
   // const { state } = useLocation();
@@ -11,27 +12,52 @@ export default function Prato() {
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
   if(!prato) {
-    return '';
+    return <NotFound/>;
   }
   return (
-    <>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>
-          {prato.title}
-        </h1>
-        <div>
-          <img src={prato.photo} alt={prato.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>
-            {prato.description}
-          </p>
-          <TagsPrato {...prato}/>
-        </div>
-      </section>
-    </>
+    // <>
+    //   <button className={styles.voltar} onClick={() => navigate(-1)}>
+    //     {'< Voltar'}
+    //   </button>
+    //   <section className={styles.container}>
+    //     <h1 className={styles.titulo}>
+    //       {prato.title}
+    //     </h1>
+    //     <div>
+    //       <img src={prato.photo} alt={prato.title} />
+    //     </div>
+    //     <div className={styles.conteudo}>
+    //       <p className={styles.conteudo__descricao}>
+    //         {prato.description}
+    //       </p>
+    //       <TagsPrato {...prato}/>
+    //     </div>
+    //   </section>
+    // </>
+    <Routes>
+      <Route path="*" element={<PaginaPadrao/>}>
+        <Route index element={
+          <>
+            <button className={styles.voltar} onClick={() => navigate(-1)}>
+              {'< Voltar'}
+            </button>
+            <section className={styles.container}>
+              <h1 className={styles.titulo}>
+                {prato.title}
+              </h1>
+              <div>
+                <img src={prato.photo} alt={prato.title} />
+              </div>
+              <div className={styles.conteudo}>
+                <p className={styles.conteudo__descricao}>
+                  {prato.description}
+                </p>
+                <TagsPrato {...prato}/>
+              </div>
+            </section>
+          </>
+        }/>
+      </Route>
+    </Routes>
   );
 }
