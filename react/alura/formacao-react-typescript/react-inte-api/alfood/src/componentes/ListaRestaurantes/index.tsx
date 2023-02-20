284a3382f5e91b163d377675014181ff66105204
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2023-02-20 17:52:24 -0300

    feat: terminando aula03 react-inte-api -- atualizar and apagar registros

--- react/alura/formacao-react-typescript/react-inte-api/alfood/src/componentes/ListaRestaurantes/index.tsx
+++ react/alura/formacao-react-typescript/react-inte-api/alfood/src/componentes/ListaRestaurantes/index.tsx
@@ -1,18 +1,25 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
+
 const ListaRestaurantes = () => {
 
-  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
-  const [proximaPagina, setProximaPagina] = useState('')
-  const [paginaAnterior, setPaginaAnterior] = useState('')
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
+  const [paginaAnterior, setPaginaAnterior] = useState('');
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
+  const [busca, setBusca] = useState('')
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
@@ -23,6 +30,19 @@ const ListaRestaurantes = () => {
       })
   }
 
+  const buscar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const opcoes = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (busca) {
+      opcoes.params.search = busca
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
   useEffect(() => {
     // obter restaurantes
     carregarDados('http://localhost:8000/api/v1/restaurantes/')
@@ -30,6 +50,10 @@ const ListaRestaurantes = () => {
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={buscar}>
+      <input type="text" value={busca} onChange={evento => setBusca(evento.target.value)} />
+      <button type='submit'>buscar</button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
     {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}>
       Página Anterior
