0f7d875cd1970ab09eb21bcd3a829f51595c053f
andre.queiroz <andre.queiroz@svatech.com.br>
2023-06-26 15:37:54 -0300

    feat: terminando aula03 nestjs-restful

---
+++ nestjs/alura/formacao-nestjs/nestjs-api-restful/loja/src/produto/dto/CriaProduto.dto.ts
@@ -0,0 +1,46 @@
+import {
+  ValidateNested,
+  IsArray,
+  IsNotEmpty,
+  Min,
+  MaxLength,
+  ArrayMinSize,
+  IsNumber,
+} from 'class-validator';
+import { CaracteristicaProdutoDTO } from './CaracteristicaProduto.dto';
+import { ImagemProdutoDTO } from './ImagemProduto.dto';
+import { Type } from 'class-transformer';
+
+export class CriaProdutoDTO {
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  nome: string;
+
+  @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+  @Min(0, { message: 'O valor com valores decimais incorretos' })
+  valor: number;
+
+  @IsNumber()
+  @Min(0, { message: 'A quantidade minima tem que ser maior que 0' })
+  quantidade: number;
+
+  @IsNotEmpty({ message: 'A descrição não pode ser vazia' })
+  @MaxLength(1000, {
+    message: 'A descrição precisa ter pelo menos 1000 caracteres',
+  })
+  descricao: string;
+
+  @ValidateNested()
+  @IsArray()
+  @Type(() => CaracteristicaProdutoDTO)
+  @ArrayMinSize(3)
+  caracteristicas: CaracteristicaProdutoDTO[];
+
+  @ValidateNested()
+  @IsArray()
+  @Type(() => ImagemProdutoDTO)
+  @ArrayMinSize(1)
+  imagens: ImagemProdutoDTO[];
+
+  @IsNotEmpty({ message: 'A categoria não pode ser vazia' })
+  categoria: string;
+}
--- nestjs/alura/formacao-nestjs/nestjs-api-restful/loja/src/usuario/usuario.controller.ts
+++ nestjs/alura/formacao-nestjs/nestjs-api-restful/loja/src/usuario/usuario.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
+import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
 
   @Post()
-  async criaUsuario(@Body() dadosUsuario) {
-    this.usuarioRepository.salvar(dadosUsuario);
-    return dadosUsuario;
+  async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    return dadosDoUsuario;
   }
 
   @Get()
