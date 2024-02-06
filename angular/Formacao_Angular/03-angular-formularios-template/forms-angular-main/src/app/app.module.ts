e55805f1b6a5edde6eac0ab2b06f8545d122af7c
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2024-02-06 10:33:23 -0300

    feat: terminando aula03 - curso 03 - formacao angular alura

--- angular/Formacao_Angular/03-angular-formularios-template/forms-angular-main/src/app/app.module.ts
+++ angular/Formacao_Angular/03-angular-formularios-template/forms-angular-main/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
+import { MensagemComponent } from './components/mensagem/mensagem.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent, MensagemComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
