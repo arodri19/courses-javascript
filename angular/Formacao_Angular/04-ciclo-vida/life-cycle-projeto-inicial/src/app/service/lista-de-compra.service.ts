09f4a016ec5662b0ea387850a224d319beb4d84d
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2024-02-23 14:29:49 -0300

    feat: terminando aula 02 - curso 04 - formacao angular - alura

--- angular/Formacao_Angular/04-ciclo-vida/life-cycle-projeto-inicial/src/app/service/lista-de-compra.service.ts
+++ angular/Formacao_Angular/04-ciclo-vida/life-cycle-projeto-inicial/src/app/service/lista-de-compra.service.ts
@@ -34,4 +34,15 @@ export class ListaDeCompraService {
   getListaDeCompra(){
     return this.listaDeCompra;
   }
+  criarItem(nomeDoItem: string){
+    const id = this.listaDeCompra.length + 1;
+    const data = new Date().toLocaleString('pt-br');
+    const comprado = false;
+    const item: Item = {id, nome: nomeDoItem, data, comprado};
+    return item;
+  }
+  adicionarItemNaLista(nomeDoItem: string){
+    const item = this.criarItem(nomeDoItem);
+    this.listaDeCompra.push(item);
+  }
 }
