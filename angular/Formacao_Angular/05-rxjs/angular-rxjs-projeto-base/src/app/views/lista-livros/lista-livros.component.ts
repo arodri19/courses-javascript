29276ee308b7a79788132db26cd2a10108da8761
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2024-02-26 21:16:17 -0300

    feat: terminando aula 01 - curso 05 - formacao angular - alura

---
+++ angular/Formacao_Angular/05-rxjs/angular-rxjs-projeto-base/src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent {
+
+  listaLivros: [];
+  campoBusca: string ='';
+
+  constructor(
+    private service: LivroService
+  ) { }
+
+  buscarLivros() {
+    this.service.buscar(this.campoBusca).subscribe(
+      (data: any) => {
+        this.listaLivros = data.items;
+      },
+      (error) => console.error(error)
+    );
+  }
+
+}
+
+
+
