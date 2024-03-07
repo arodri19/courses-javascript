d77bbeb6fab22b09c7a9c31bda3779d0af5b251b
ANDRE RODRIGUES QUEIROZ <andre.rodrigues19@outlook.com.br>
2024-03-07 16:18:20 -0300

    feat: terminando aula 04 - curso 06 - formacao angular - alura

--- angular/Formacao_Angular/06-animacoes/2806-animacoes-projeto-base/src/app/animations.ts
+++ angular/Formacao_Angular/06-animacoes/2806-animacoes-projeto-base/src/app/animations.ts
@@ -2,16 +2,33 @@ import { animate, state, style, transition, trigger } from "@angular/animations"
 
 export const highlightedStateTrigger = trigger('highlightedState', [
     state('default', style({
-      border: '2px solid #B2B6FF',
+        border: '2px solid #B2B6FF',
     })),
     state('highlighted', style({
-      border: '4px solid #B2B6FF',
-      filter: 'brightness(92%)'
+        border: '4px solid #B2B6FF',
+        filter: 'brightness(92%)'
     })),
     transition('default => highlighted', [
-      animate('200ms ease-out',style({
-        transform: 'scale(1.02)'
-      })),
-      animate(200)
+        animate('200ms ease-out', style({
+            transform: 'scale(1.02)'
+        })),
+        animate(200)
     ])
-  ])
+])
+
+export const shownStateTrigger = trigger('shownState', [
+    transition(':enter', [
+        style({
+            opacity: 0,
+        }),
+        animate('300ms ease-out', style({
+            opacity: 1,
+            transform: 'translateX(0)'
+        }))
+    ]),
+    transition(':leave', [
+        animate(300, style({
+            opacity: 0,
+        }))
+    ])
+])
